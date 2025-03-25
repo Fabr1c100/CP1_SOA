160092cf3e381c56d578402989a55b388e536d06
Fabrício G. Saavedra <126624791+Fabr1c100@users.noreply.github.com>
2025-03-24 21:37:45 -0300

    Add files via upload

---
+++ study-apiw/src/main/java/com/github/acnaweb/study_apiw/service/ProdutoService.java
@@ -0,0 +1,18 @@
+package com.github.acnaweb.study_apiw.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import com.github.acnaweb.study_apiw.model.Produto;
+
+@Service
+public class ProdutoService {
+    private List<Produto> produto = new ArrayList();
+    private Long id = 1L;
+    
+    public Produto save(Produto produto){
+        produto.setId(id);
+        return produto;
+    }
+}
